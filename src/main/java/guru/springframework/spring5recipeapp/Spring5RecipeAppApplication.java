4dc83fda8281ef89efd5cdb242ab88ed5edbc75a
Prakshal26 <jprakshal2607@gmail.com>
2020-06-20 16:46:15 +0530

    One to many and many to one completed

--- src/main/java/guru/springframework/spring5recipeapp/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/spring5recipeapp/Spring5RecipeAppApplication.java
@@ -1,3 +1,14 @@
+/*
+
+ENtity Relationship
+Recipe to Notes-----One to One
+Recipe to cateogry --- Many to many
+recipe to Ingridients ----One to many
+
+Ingridents to unit of measure -----one to one
+
+ */
+
 package guru.springframework.spring5recipeapp;
 
 import org.springframework.boot.SpringApplication;
---
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
@@ -0,0 +1,55 @@
+package guru.springframework.spring5recipeapp.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private BigDecimal amount;
+
+    /*
+    In Recipe class we have given One to Many that means One Recipe can have many ingridents.
+    But here we are giving many to one as all these ingrident belongs to one recipe.
+    No need of cascade as we dont want to retain ingredents if recipe is gone.
+     */
+    @ManyToOne
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
